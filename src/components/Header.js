e1a914f799cb49ee401570bf01416461a9b01904
Junior <juniormbaya243@gmail.com>
2020-11-06 13:15:30 +0000

    add header

---
+++ src/components/Header.js
@@ -0,0 +1,43 @@
+import React from "react";
+import "../App.css";
+
+import ShoppingCartIcon from "@material-ui/icons/ShoppingCart";
+import AccountCircleIcon from "@material-ui/icons/AccountCircle";
+import { makeStyles } from "@material-ui/core/styles";
+
+import logo from "../assets/logo.webp";
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    "& > svg": {
+      margin: theme.spacing(2),
+    },
+  },
+  logoColor: {
+    color: "red",
+  },
+}));
+
+function Header() {
+  const classes = useStyles();
+  return (
+    <div className="header__contain">
+      <div className="header">
+        <img className="header__logo" src={logo} alt="company logo" />
+        <div className="header__icon">
+          <div className="header__iconAccount">
+            <AccountCircleIcon fontSize="large" color="secondary" />
+          </div>
+          <div className="header__iconCart">
+            <ShoppingCartIcon fontSize="large" color="secondary" />
+          </div>
+        </div>
+      </div>
+      <div className="header__sousHeader">
+        <h2>Test</h2>
+      </div>
+    </div>
+  );
+}
+
+export default Header;
