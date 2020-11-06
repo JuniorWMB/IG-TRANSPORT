803b8123e7f5d985bb128feb05708532dea56374
Junior <juniormbaya243@gmail.com>
2020-11-06 23:05:23 +0000

    add some stuf

--- src/Pages/SignUp.js
+++ src/Pages/SignUp.js
@@ -1,7 +1,15 @@
 import React from "react";
 // import { Formik } from "formik";
 import Header from "../components/Header";
+import PhoneIphoneIcon from "@material-ui/icons/PhoneIphone";
+import LocationOnIcon from "@material-ui/icons/LocationOn";
+import EmailIcon from "@material-ui/icons/Email";
+import FacebookIcon from "@material-ui/icons/Facebook";
+import LinkedInIcon from "@material-ui/icons/LinkedIn";
+import InstagramIcon from "@material-ui/icons/Instagram";
+
 import "../App.css";
+import logo from "../assets/logo.webp";
 
 function SignUp() {
   return (
@@ -12,7 +20,7 @@ function SignUp() {
         <form className="form__signdesign" action="">
           <div className="form__title">
             <h1>Créer votre compte</h1>
-            <div>logo</div>
+            <img style={{ height: "80%" }} src={logo} alt="company logo" />
           </div>
           <div className="form__block">
             <div className="form__designleft">
@@ -27,12 +35,39 @@ function SignUp() {
               <label htmlFor="">Mot de passe</label>
               <input type="password" />
               <label htmlFor="">Confirmation de votre mot de passe</label>
-              <input style={{ marginBottom: "60px" }} type="password" />
+              <input style={{ marginBottom: "48px" }} type="password" />
               <button type="submit">VALIDER</button>
             </div>
           </div>
         </form>
-        <div className="form__contact"></div>
+        <div className="form__contact">
+          <div className="form__info">
+            <div className="form__infoabove">
+              <LocationOnIcon />
+              <p>16 rue du Cantal 91090 Lisses</p>
+            </div>
+            <div className="form__infoabove">
+              <PhoneIphoneIcon />
+              <p>0642424242</p>
+            </div>
+            <div className="form__infoabove">
+              <EmailIcon />
+              <p>Contact@iguanetransport.com</p>
+            </div>
+          </div>
+          <div
+            style={{
+              marginLeft: "20px",
+              display: "flex",
+              justifyContent: "space-between",
+              width: "40%",
+            }}
+          >
+            <FacebookIcon />
+            <LinkedInIcon />
+            <InstagramIcon />
+          </div>
+        </div>
       </div>
     </div>
   );
