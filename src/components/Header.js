096bab82b109ed27de499a6598244621a750f928
Junior <juniormbaya243@gmail.com>
2020-11-12 18:09:42 +0000

    fix issues

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,26 +1,12 @@
 import React from "react";
 import "../App.css";
-
-import ShoppingCartIcon from "@material-ui/icons/ShoppingCart";
-import AccountCircleIcon from "@material-ui/icons/AccountCircle";
-import { makeStyles } from "@material-ui/core/styles";
+import { faUser, faShoppingCart } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 import logo from "../assets/logo.webp";
 import { Link } from "react-router-dom";
 
-const useStyles = makeStyles((theme) => ({
-  root: {
-    "& > svg": {
-      margin: theme.spacing(2),
-    },
-  },
-  logoColor: {
-    color: "red",
-  },
-}));
-
 function Header() {
-  const classes = useStyles();
   return (
     <div className="header__contain">
       <div className="header">
@@ -28,11 +14,34 @@ function Header() {
           <img className="header__logo" src={logo} alt="company logo" />
         </Link>
         <div className="header__icon">
-          <Link to="/signup" className="header__iconAccount">
-            <AccountCircleIcon fontSize="large" color="primary" />
+          <Link
+            to="/signup"
+            className="header__iconAccount"
+            style={{
+              background: "#94c935",
+              // background: "#2C436D",
+              fontSize: "30px",
+              border: "1px solid black",
+              color: "white",
+              border: "none",
+            }}
+          >
+            {/* <AccountCircleIcon fontSize="large" color="primary" /> */}
+            <FontAwesomeIcon icon={faUser} />
           </Link>
-          <div className="header__iconCart">
-            <ShoppingCartIcon fontSize="large" color="primary" />
+          <div
+            className="header__iconCart"
+            style={{
+              background: "#94c935",
+              // background: "#2C436D",
+              fontSize: "30px",
+              border: "1px solid black",
+              color: "white",
+              border: "none",
+            }}
+          >
+            {/* <ShoppingCartIcon fontSize="large" color="primary" /> */}
+            <FontAwesomeIcon icon={faShoppingCart} />
           </div>
         </div>
       </div>
