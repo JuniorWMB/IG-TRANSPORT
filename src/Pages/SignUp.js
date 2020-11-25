90cb0b2ad25d4b98c9e3d7e38b2e20447ff8f452
Junior <juniormbaya243@gmail.com>
2020-11-25 15:14:23 +0000

    add style

--- src/Pages/SignUp.js
+++ src/Pages/SignUp.js
@@ -1,10 +1,13 @@
 import React, { useState } from "react";
 // import { Formik } from "formik";
 import Header from "../components/Header";
+import zxcvbn from "zxcvbn";
 
 import "../App.css";
-import logo from "../assets/logo.webp";
 import gsap from "gsap";
+/*eslint-disable*/
+const passwordReg = /^[a-zA-Z0-9_\.\-\@\#\$\%]{4}$/;
+const emailReg = /^([a-zA-Z0-9_\.]+)@([a-zA-Z0-9_\-\.]+)\.([a-zA-Z]{2,5})$/;
 
 function SignUp() {
   const [name, setName] = useState("");
@@ -13,6 +16,7 @@ function SignUp() {
   const [password, setPassword] = useState("");
   const [confirmPassword, setConfirmPassword] = useState("");
   const [textError, setTextError] = useState("");
+  const [validationMp, setValidationMp] = useState(true);
   const [errorStyle, setErrorStyle] = useState(true);
 
   const handleSubmit = (e) => {
@@ -30,6 +34,11 @@ function SignUp() {
           duration: 1,
           boxShadow: "1px 1px 20px green",
         });
+        if (
+          password.charAt(e.target.value) &&
+          confirmPassword.charAt(e.target.value) === ""
+        ) {
+        }
       } else {
         setTextError("Les mots de passes ne sont pas identique");
         setErrorStyle(false);
@@ -45,6 +54,53 @@ function SignUp() {
       });
     }
   };
+
+  const testResult = zxcvbn(password);
+  const num = (testResult.score * 100) / 4;
+  const textProgressColor = () => {
+    switch (testResult.score) {
+      case 0:
+        return "Tres faible";
+      case 1:
+        return "Faible";
+      case 2:
+        return "Moyen";
+
+      case 3:
+        return "Fort";
+
+      case 4:
+        return "#Très fort";
+      default:
+        return "";
+    }
+  };
+
+  const funcProgressColor = () => {
+    switch (testResult.score) {
+      case 0:
+        return "#828282";
+      case 1:
+        return "#EA1111";
+      case 2:
+        return "#FFAd00";
+
+      case 3:
+        return "#9bc158";
+
+      case 4:
+        return "#00b500";
+      default:
+        return "none";
+    }
+  };
+
+  const changePasswordColor = () => ({
+    width: `${num}%`,
+    background: funcProgressColor(),
+    height: "100%",
+    // borderRadius: "15px",
+  });
 
   return (
     <div style={{ position: "relative" }}>
@@ -92,7 +148,10 @@ function SignUp() {
               }}
             />
           </div>
-          <div className="block__input">
+          <div
+            className="block__input"
+            style={{ display: "flex", flexDirection: "column" }}
+          >
             <label htmlFor="Password"></label>
             <input
               className={errorStyle ? "form__bottominput" : "form__blockerror"}
@@ -104,6 +163,7 @@ function SignUp() {
               }}
             />
           </div>
+
           <div className="block__input">
             <label htmlFor="Confirm password"></label>
             <input
@@ -119,28 +179,62 @@ function SignUp() {
           <div
             style={{
               display: "flex",
+              flexDirection: "column",
+              alignItems: "center",
+              position: "absolute",
+              top: "313px",
+              left: "25.5%",
+            }}
+          >
+            <div
+              style={{
+                width: "305px",
+                height: "10px",
+                border: "0px solid grey",
+                marginTop: "1px",
+                borderRadius: "15px",
+                background: "grey",
+                overflow: "hidden",
+              }}
+            >
+              <div className="progress_bar" style={changePasswordColor()}></div>
+            </div>
+            <div
+              style={{
+                width: "300px",
+                display: "flex",
+                justifyContent: "flex-end",
+                marginTop: "5px",
+              }}
+            >
+              <h3>weak</h3>
+            </div>
+          </div>
+          {/* <div
+            style={{
+              display: "flex",
               justifyContent: "center",
               alignItems: "center",
               height: "100px",
               flexDirection: "column",
-              border: "1px solid red",
-              textAlign: "center",
               color: "grey",
             }}
           >
-            <p className="text__confirm">
-              Votre mots de passe doit contenir une majuscule
-            </p>
-            <p className="text__confirm">
-              Votre mots de passe doit contenir une minuscule
-            </p>
-            <p className="text__confirm">
-              Votre mots de passe doit contenir un caracterer special
-            </p>
-            <p className="text__confirm">
-              Votre mots de passe doit contenir un chiffre
-            </p>
-          </div>
+            <div style={{ width: "300px" }}>
+              <p className="text__confirm">
+                Votre mots de passe doit contenir une majuscule.
+              </p>
+              <p className="text__confirm">
+                Votre mots de passe doit contenir une minuscule.
+              </p>
+              <p className="text__confirm">
+                Votre mots de passe doit contenir un caracterer special.
+              </p>
+              <p className="text__confirm">
+                Votre mots de passe doit contenir au moins un chiffre.
+              </p>
+            </div>
+          </div> */}
           <div
             style={{
               display: "flex",
