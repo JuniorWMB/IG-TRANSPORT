0d4e9cd203e5b652741e19e73d33bff09f2aa3c2
Junior <juniormbaya243@gmail.com>
2021-02-22 14:01:36 +0100

    add logique

--- src/components/BoxMobileBlock.js
+++ src/components/BoxMobileBlock.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 
 const BoxMobileBlock = ({
   titleQuatre,
@@ -9,6 +9,7 @@ const BoxMobileBlock = ({
   etage,
   total,
 }) => {
+  const [show, setShow] = useState(false);
   return (
     <div className="boxmobiletext__block">
       <h4>
@@ -18,38 +19,53 @@ const BoxMobileBlock = ({
         <div className="boxmobiletext__livraison">
           <p> {livraison} </p>
         </div>
-        <div className="boxmobile__show">
-          <div className="boxmobiletext__manutention">
-            <p>Voulez-vous de la manutention ?</p>
-            <div className="boxmobiletext__button">
-              <button>OUI</button>
-              <button>NON</button>
-            </div>
-          </div>
-          <div className="boxmobile__portage">
-            <p>{portage}</p>
-            <select name="" id="">
-              <option value="20">10m à 20m</option>
-              <option value="40">20m à 40m</option>
-              <option value="60">40m à 60m</option>
-              <option value="80">+ de 80m</option>
-            </select>
-          </div>
-          <div className="boxmobile__etage">
-            <p>{etage}</p>
-            <select name="" id="">
-              <option value="1">1</option>
-              <option value="2">2</option>
-              <option value="3">3</option>
-              <option value="4">4</option>
-              <option value="5">5</option>
-              <option value="6">6</option>
-            </select>
+
+        <div className="boxmobiletext__manutention">
+          <p>Voulez-vous de la manutention ?</p>
+          <div className="boxmobiletext__button">
+            <button
+              onClick={() => {
+                setShow(true);
+              }}
+            >
+              OUI
+            </button>
+            <button
+              onClick={() => {
+                setShow(false);
+              }}
+            >
+              NON
+            </button>
           </div>
         </div>
+        {show ? (
+          <div className="boxmobile__show">
+            <div className="boxmobile__portage">
+              <p>{portage}</p>
+              <select name="" id="">
+                <option value="20">10m à 20m</option>
+                <option value="40">20m à 40m</option>
+                <option value="60">40m à 60m</option>
+                <option value="80">+ de 80m</option>
+              </select>
+            </div>
+            <div className="boxmobile__etage">
+              <p>{etage}</p>
+              <select name="" id="">
+                <option value="1">1</option>
+                <option value="2">2</option>
+                <option value="3">3</option>
+                <option value="4">4</option>
+                <option value="5">5</option>
+                <option value="6">6</option>
+              </select>
+            </div>
+          </div>
+        ) : null}
         <div className="boxmobile__total">
           <p>{total}</p>
-          <span>100€</span>
+          <span> 100€</span>
         </div>
       </div>
     </div>
